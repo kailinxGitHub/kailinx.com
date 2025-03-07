91cf6ce23190c4b124b2e26869d7544d55598018
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2025-03-07 04:09:47 -0500

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Kailin Xing Portfolio",
+  title: "Kailin Xing",
   description: "Professional portfolio of Kailin Xing, Software Engineer"
 }
 
